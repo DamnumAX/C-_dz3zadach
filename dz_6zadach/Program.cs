72511747d8b585964dc99b996cfbb5af3afc965c
Filipp G <filfilet@mail.ru>
2022-11-30 17:11:40 +0300

    first commit

---
+++ dz_6zadach/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());;
+
+if (num % 2 == 1)
+{
+Console.WriteLine("Число " + num + " является: нечетным");
+}
+else
+{
+Console.WriteLine("Число " + num + "является: чётным");
+}
